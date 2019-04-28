6dd49dc5992e72594204e05c77c863871663707d
Mitrou <mahnosam@gmail.com>
2019-04-28 23:58:01 +0300

    mock for jenkins

--- 111/mock_1loginn_est.py
+++ 111/mock_1loginn_est.py
@@ -1,10 +1,8 @@
-from selenium import webdriver
 import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from toolbelt import base_url_mock
-import allure
 
 
 class TestWebSmoke:
